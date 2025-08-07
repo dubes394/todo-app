4c672324077c846c0ffc1ebd82b496530351bd5a
kunal <kunaldubey246@gmail.com>
2025-08-07 16:09:12 -0400

    Added clock, Changed theme, handle edit and complete error

--- gui.py
+++ gui.py
@@ -1,5 +1,9 @@
 import functions
 import FreeSimpleGUI as sg 
+import time 
+
+sg.theme("Black")
+clock =sg.Text("", key="clock")
 
 label = sg.Text("Type in a to-do")
 input_box = sg.InputText(tooltip="Enter todo", key="todo")
@@ -11,17 +15,16 @@ complete_button = sg.Button("Complete")
 exit_button = sg.Button("Exit")
 
 window = sg.Window("My To-Do App",
-                   layout=[[label],
+                   layout=[[clock],
+                           [label],
                            [input_box, add_button],
                            [list_box, edit_button, complete_button], 
                            [exit_button]],
                    font=("Helvetica", 18))
 
 while True:
-    event, values  = window.read()      #*event tells you what happened (e.g., "Add" or "Edit"), values holds what the user typed or selected
-    print(1, event)
-    print(2, values)
-    print(3, values["todos"])
+    event, values  = window.read(timeout=200)      #*event tells you what happened (e.g., "Add" or "Edit"), values holds what the user typed or selected
+    window["clock"].update(value=time.strftime("%b %d, %Y %H:%H:%S"))
     match event:
         case "Add":
             todos = functions.get_todos()         # Load current todos
@@ -30,21 +33,27 @@ while True:
             functions.write_todos(todos)          # Save updated list
             window["todos"].update(values=todos)
         case "Edit":
-            todos_to_edit = values["todos"][0]           # List of selected todos (always a list)
-            new_todo = values["todo"]               # Get new value from input
-            
-            todos = functions.get_todos()               # Load all todos again            
-            index = todos.index(todos_to_edit)                  # Find selected item in the list
-            todos[index] = new_todo                       # Replace it
-            functions.write_todos(todos)                  # Save back to file
-            window["todos"].update(values=todos)          # Refresh listbox in UI
+            try:
+                todos_to_edit = values["todos"][0]           # List of selected todos (always a list)
+                new_todo = values["todo"]               # Get new value from input
+                
+                todos = functions.get_todos()               # Load all todos again            
+                index = todos.index(todos_to_edit)                  # Find selected item in the list
+                todos[index] = new_todo                       # Replace it
+                functions.write_todos(todos)                  # Save back to file
+                window["todos"].update(values=todos)          # Refresh listbox in UI
+            except IndexError:
+                sg.popup("Please select an item first", font=("Helvatica", 20))
         case "Complete":
-            todos_to_complete = values["todos"][0]  #List down the current todos we have
-            todos = functions.get_todos()
-            todos.remove(todos_to_complete)     #using remove method we remove todos we have completed
-            functions.write_todos(todos)        #and using write functions we write the todos
-            window["todos"].update(values=todos) #update the todos in the window
-            window["todo"].update(value=" ")
+            try:
+                todos_to_complete = values["todos"][0]  #List down the current todos we have
+                todos = functions.get_todos()
+                todos.remove(todos_to_complete)     #using remove method we remove todos we have completed
+                functions.write_todos(todos)        #and using write functions we write the todos
+                window["todos"].update(values=todos) #update the todos in the window
+                window["todo"].update(value=" ")
+            except IndexError:
+                sg.popup("Please select an item first", font=("Helvatica", 20))
         case "Exit":
             break            
         case "todos" :
